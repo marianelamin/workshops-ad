bd72ca5a4e357604a6a80319f50af4c359614013
Marianela <marianela_min@yahoo.com>
2019-03-13 20:36:50 -0500

    almost finalized the website to publish workshops info

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+
 const facilitators = [
     {
-        id:1,
+        id:0,
         name: "Marianela",
         email:"mmendozamend@neiu.edu"
     },
     {
-        id:2,
+        id:1,
         name: "Erik",
         email:"e-macias@neiu.edu"
     },
     {
-        id:3,
+        id:2,
         name: "John",
         email:"j-mcrissman@neiu.edu"
     }
@@ -18,31 +19,31 @@ const facilitators = [
 
 const workshops = [
     {
-        id:1,
-        name: 'commandLine',
-        whatIllLearn: 'a paragraph briefly explaining',
+        workshopID:1,
+        name: 'Command Line - Bash',
+        whatIllLearn: 'how to navigate through directories, create, modify and delete files, run java programs using only the command line.  We will be using Bash as the scripting language.',
         whatToBring: 'Laptop',
-        when:'',
-        where:'',
-        time:'',
-        regLink: '',
-        moreInfoLink: '',
-        facilitatorsID:[1,2]
+        when:'March 14th, 2019',
+        where:'LWH 2044',
+        time:'3pm - 4pm',
+        regLink: 'https://docs.google.com/forms/d/e/1FAIpQLScwm5oaYSdvA4hnPCg_tiTBgD5bbPg-WNNW61a7r9eq9YtHNw/viewform?usp=sf_link',
+        moreInfoLink: 'http://manela.web.ve/workshops/command-line-git-bash/CommandLineWorkshop.pdf',
+        facilitatorsID:[0,1]
     },
     {
-        id:2,
-        name: 'Kali Linux and hiding stuff',
-        whatIllLearn: 'a paragraph briefly explaining',
+        workshopID:2,
+        name: 'Steganography and Kali Linux',
+        whatIllLearn: 'how to install a program that we will be using in Kali Linux, as well as how to embed a text file, audio file and a java program in an image file.  You will learn also how to decrypt that image file with all the data stored.  Kali Linux is widely used by security experts in the real world.',
         whatToBring: 'Laptop',
-        when:'',
-        where:'',
-        time:'',
-        regLink: '',
-        moreInfoLink: '',
-        facilitatorsID:[1,2]
+        when:'March 28th, 2019',
+        where:'LWH 2044',
+        time:'3pm - 4pm',
+        regLink: 'https://docs.google.com/forms/d/e/1FAIpQLSfquBGruEUW2XLalKC3JS3UszGDE0-CHz6rEqdEbZ7U49fuwQ/viewform?usp=sf_link',
+        moreInfoLink: 'http://www.cahsi-midwest.org/workshop1-security/index.htm',
+        facilitatorsID:[1]
     },
     {
-        id:3,
+        workshopID:3,
         name: 'html and more',
         whatIllLearn: 'a paragraph briefly explaining',
         whatToBring: 'Laptop',
@@ -51,28 +52,37 @@ const workshops = [
         time:'',
         regLink: '',
         moreInfoLink: '',
-        facilitatorsID:[1,2]
+        facilitatorsID:[0,1]
     }
 ];
 
 
-let aCard =  (workshop) => {
-   return  `<ul class="row">
-            <div class="col-md-8">
-                <li class="col-12"><label class="font-weight-bold">name</label> <span class="">${workshop[name]}</span> </li>
-                <li class="col-md-12"><label class="font-weight-bold">whatIllLearn:</label> <span class="">${workshop[whatIllLearn]}</span> </li>
-                <li class="col-md-12"><label class="font-weight-bold">whatToBring:</label> <span class="">${workshop[whatToBring]} <span> </li>
-                <li class="col-12"><label class="font-weight-bold">facilitatorsID:</label> <span class="">  foorloop on {facilitators[ {facilitatorsID}]}----</span> </li>
+const aCard =  (workshop) => {
+   return  `<li> <ul class="row">
+            <div class="col-lg-8">
+                <li class="col-12 my-1"><label class="font-weight-bold">Name: </label> <span class="">${workshop['name']}</span> </li>
+                <li class="col-md-12 my-1"><label class="font-weight-bold">What to Learn: </label><p class="">${workshop['whatIllLearn']} <a target="_blank" href="${workshop['moreInfoLink']}" title="more about the workshop">(more)</a></p></li>
+                <li class="col-md-12 my-1"><label class="font-weight-bold">What to bring: </label> <span class="">${workshop['whatToBring']} <span> </li>
+                <li class="col-12 my-1"><label class="font-weight-bold">FacilitatorsID:</label> <span class="">  ${workshop['facilitatorsID'].reduce((total,val) => total+facilitators[val].name+', '," ")}--</span> </li>
             </div>
             <div class="col-md-auto">
-                <li class="col"><label class="font-weight-bold">when:</label> <span class="">${workshop[when]}</span> </li>
-                <li class="col"><label class="font-weight-bold">where:</label> <span class="">${workshop[where]}</span> </li>
-                <li class="col"><label class="font-weight-bold">time:</label> <span class="">${workshop[time]}</span> </li>
-                <li class="col-12"><label class="font-weight-bold"> <a href="${workshop[moreInfoLink]}" title="more about the workshop"> moreInfoLink:</a></label> <span class=""></span> </li>
-                <li class="col-12"><label class="font-weight-bold"><a href="${workshop[regLink]}" role="btn" title = "register to attend" class="btn btn-primary">regLink:</a></label> <span class=""><span> </li>
+                <li class="col"><label class="font-weight-bold">When:</label> <span class="">${workshop['when']}</span> </li>
+                <li class="col"><label class="font-weight-bold">Where:</label> <span class="">${workshop['where']}</span> </li>
+                <li class="col"><label class="font-weight-bold">Time:</label> <span class="">${workshop['time']}</span> </li>
+                <li class="col-12"><label class="font-weight-bold"><a target="_blank" href="${workshop['regLink']}" role="btn" title = "register to attend" class="btn btn-primary">Register</a></label> <span class=""><span> </li>
+                <li class="col-12"><label class="font-weight-bold"> </label> <span class=""></span> </li>
             </div>
-            </ul>   `;
+            </ul></li>
+            `;
     
 };
 
-document.querySelector('#schedule-container').innerHTML += '<p>this is a test</p>'+aCard;
+
+let content_list = workshops.reduce((total,value,item)=>{
+    console.log(value['name']+" "+item);
+    return total + aCard(value);
+
+},"");
+
+
+document.querySelector('#list-1').innerHTML += content_list;
