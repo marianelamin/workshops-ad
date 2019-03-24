c1b071499e0eac99bdeeca025b091599b3b93e8b
Marianela <marianela_min@yahoo.com>
2019-03-24 12:14:21 -0500

    updated dates

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const workshops = [
         facilitatorsID:[1,2]
     },
     {
-        name: 'Steganography and Kali Linux',
+        name: 'Steganography and Kali Linux: Part 1',
         whatIllLearn: 'How to install a program that we will be using in Kali Linux, as well as how to embed a text file, audio file and a java program in an image file.  You will learn also how to decrypt that image file with all the data stored.  Kali Linux is widely used by security experts in the real world.',
         whatToBring: 'Laptop',
         when:'March 28th, 2019',
@@ -53,10 +53,46 @@ const workshops = [
         facilitatorsID:[2]
     },
     {
+        name: 'Steganography and Kali Linux: Part 2',
+        whatIllLearn: 'How to install a program that we will be using in Kali Linux, as well as how to embed a text file, audio file and a java program in an image file.  You will learn also how to decrypt that image file with all the data stored.  Kali Linux is widely used by security experts in the real world.',
+        whatToBring: 'Laptop',
+        when:'April 4th, 2019',
+        where:'LWH 2044',
+        time:'3pm - 4pm',
+        regLink_state: true,
+        regLink: 'https://docs.google.com/forms/d/e/1FAIpQLSeJRddOIsxJox1Dh6eghja3GLXxhOnWaurTPhdgtgFGFzRmrQ/viewform?usp=sf_link',
+        moreInfoLink: 'http://www.cahsi-midwest.org/workshop1-security/index.htm',
+        facilitatorsID:[2]
+    },
+    {
+        name:'Python on the cloud',
+        whatIllLearn: `We will learn the basics of python syntax.  We will execute python code from the cloud using GENI.`,
+        whatToBring: 'Laptop',
+        when:'April 2nd, 2019',
+        where:'LWH 2031',
+        time:'3pm - 4:30pm',
+        regLink_state: true,
+        regLink: 'https://docs.google.com/forms/d/e/1FAIpQLSddK7D1TxFjIDZBHwirACiO5qkcbfAAW58u1pLRtp9s5oDbGg/viewform?usp=sf_link',
+        moreInfoLink: '',
+        facilitatorsID:[1]
+    },
+    {
+        name:'Artificial Neural Networks using Jupiter, python and Sci Kit Learn.',
+        whatIllLearn: 'You will apply AI concepts on ...',
+        whatToBring: 'Laptop',
+        when:'April 4th, 2019',
+        where:'LWH 2031',
+        time:'9am - 11am',
+        regLink_state: false,
+        regLink: '',
+        moreInfoLink: '',
+        facilitatorsID:[3]
+    },
+    {
         name: 'CS200 for Javascript',
         whatIllLearn: 'We will contribute to an existent project, using online git repositories and applying concepts of Javascript.  We will start with the foundations and build up on it.',
         whatToBring: 'Laptop',
-        when:'TBD - April week 1',
+        when:'TBD',
         where:'TBD',
         time:'TBD',
         regLink_state: false,
@@ -68,38 +104,14 @@ const workshops = [
         name: 'HTML/CSS - Canvas and Bootstrap',
         whatIllLearn: `Using html/css and Javascript to build a web application, a game or a calculator.  Once it is finished, we will deploy it to a server where everyone will be able to see/use it.  For this we will learn how to use Boostrap CSS framework in order to add style.  At the end we will save changes on the online reposotory.`,
         whatToBring: 'Laptop, Create a GitHub or Bitbucket account, Email <a href="mailto:helpdesk@neiu.edu">Helpdesk NEIU</a> to request a space on homepages.neiu.edu in order to host our application',
-        when:'TBD - April week 1',
+        when:'TBD',
         where:'TBD',
         time:'TBD',
         regLink_state: false,
         regLink: '',
         moreInfoLink: '',
         facilitatorsID:[0]
-    },
-    {
-        name:'CS200 for Python and Computer Networks',
-        whatIllLearn: `We will learn the basics of python in order to write translate Java programs into Python.`,
-        whatToBring: 'Laptop',
-        when:'TBD - April week 2',
-        where:'TBD',
-        time:'TBD',
-        regLink_state: false,
-        regLink: '',
-        moreInfoLink: '',
-        facilitatorsID:[1]
-    },
-    {
-        name:'Artificial Neural Networks using Jupiter, python and Sci Kit Learn.',
-        whatIllLearn: 'You will apply AI concepts on ...',
-        whatToBring: 'Laptop',
-        when:'TBD - April week 2',
-        where:'TBD',
-        time:'TBD',
-        regLink_state: false,
-        regLink: '',
-        moreInfoLink: '',
-        facilitatorsID:[3]
-    },
+    }
 ];
 
 
